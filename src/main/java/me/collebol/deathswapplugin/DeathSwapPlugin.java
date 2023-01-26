512fecc64aa966bd6c2ddb1708a1945daaaf500c
lalaMakkertje <67544089+ColleBoll@users.noreply.github.com>
2023-01-26 13:57:03 +0100

    first init

---
+++ src/main/java/me/collebol/deathswapplugin/DeathSwapPlugin.java
@@ -0,0 +1,20 @@
+package me.collebol.deathswapplugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DeathSwapPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Death Swap plugin is on! Made by: ColleBol | play.ludocraft.net");
+
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
